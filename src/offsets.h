5f049cbe428cc841e6f75f2a2f887367a58f3616
avitran0 <holyhades64@gmail.com>
2024-10-02 23:09:12 +0200

    add some offsets

--- src/offsets.h
+++ src/offsets.h
@@ -3,17 +3,52 @@
 
 #include "types.h"
 
-struct LibraryOffsets {
+typedef struct LibraryOffsets {
     u64 client;
-};
+} LibraryOffsets;
 
-struct GeneralOffsets {
+typedef struct GeneralOffsets {
     u64 entity_list;
-};
+    u64 sdl_window;
+} GeneralOffsets;
+
+typedef struct InterfaceOffsets {
+    u64 convar;
+} InterfaceOffsets;
+
+typedef struct DirectOffsets {
+    u64 local_controller;
+    u64 view_matrix;
+    u64 button_state;
+} DirectOffsets;
+
+typedef struct ConvarOffsets {
+    u64 sensitivity;
+    u64 teammates_are_enemies;
+} ConvarOffsets;
+
+// todo: finish these offsets
+typedef struct ControllerOffsets {
+    u64 pawn;
+} ControllerOffsets;
+
+typedef struct PawnOffsets {
+    u64 game_scene_node;
+} PawnOffsets;
+
+typedef struct GameSceneNodeOffsets {
+    u64 dormant;
+} GameSceneNodeOffsets;
 
 typedef struct Offsets {
-    struct LibraryOffsets library;
-    struct GeneralOffsets general;
+    LibraryOffsets library;
+    GeneralOffsets general;
+    InterfaceOffsets interface;
+    DirectOffsets direct;
+
+    ControllerOffsets controller;
+    PawnOffsets pawn;
+    GameSceneNodeOffsets game_scene_node;
 } Offsets;
 
 #endif
