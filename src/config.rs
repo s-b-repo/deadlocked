371906e2ed2dba09a1e39ee4fe3843bf263c1154
avitran0 <holyhades64@gmail.com>
2024-10-22 10:01:16 +0200

    remove mouse debugging

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ use strum::IntoEnumIterator;
 
 use crate::{key_codes::KeyCode, message::Game};
 
-pub const DEBUG_WITHOUT_MOUSE: bool = true;
+pub const DEBUG_WITHOUT_MOUSE: bool = false;
 
 const REFRESH_RATE: u64 = 1000;
 pub const LOOP_DURATION: Duration = Duration::from_millis(1000 / REFRESH_RATE);
