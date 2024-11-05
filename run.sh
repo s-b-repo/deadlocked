1bad4445ceae226a108e8b9512a659e5622b4ccf
avitran0 <holyhades64@gmail.com>
2024-11-05 18:13:47 +0100

    add font

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
-cargo build --release
+cargo build --release &>/dev/null
 
 TARGET_DIR="$SCRIPT_DIR/target/release"
 cd "$TARGET_DIR"
-sudo ./deadlocked
+./deadlocked
